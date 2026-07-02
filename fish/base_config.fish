5000441506a8895d496b3540e87e109341094c3f
Schuyler Mortimer <schuymortimer@gmail.com>
2026-07-02 11:05:32 -0700

    fix fish colors

--- fish/base_config.fish
+++ fish/base_config.fish
@@ -1,5 +1,34 @@
 set -gx EDITOR nvim
 
+# Color scheme (declarative, replaces machine-local fish_variables sync)
+set -g fish_color_autosuggestion brblack
+set -g fish_color_cancel -r
+set -g fish_color_command normal
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end green
+set -g fish_color_error brred
+set -g fish_color_escape brcyan
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_host_remote yellow
+set -g fish_color_normal normal
+set -g fish_color_operator brcyan
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection cyan --bold
+set -g fish_color_search_match white --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description yellow -i
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_selected_background -r
+
 # User paths (machine-local, kept out of fish_user_paths sync)
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
